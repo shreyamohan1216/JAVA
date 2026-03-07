17b0ed1d93032b325f72bb13b3fc4e766aabaf80
sirisurabhi1406-code <sirisurabhi1406@gmail.com>
2026-03-07 09:00:15 +0530

    initial

--- src/dsa/collections/Printer.java
+++ src/dsa/collections/Printer.java
@@ -12,7 +12,7 @@ public class Printer<T> {//T-template Or Generic  datatype//We can say anything 
         Printer<Integer>integerPrinter=new Printer<>(1);
         Printer<String>stringPrinter=new Printer<>("Java");
         Printer<Float>floatPrinter=new Printer<>(1.2f);
-        Printer<Double>doublePrinter=new Printer<>(2.345567);//can include d elsestill works
+        Printer<Double>doublePrinter=new Printer<>(2.345567);//can include d else still works
         Printer<Boolean>booleanPrinter=new Printer<>(true);
         stringPrinter.printValue();
         booleanPrinter.printValue();
